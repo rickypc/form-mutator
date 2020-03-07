3773353b113bfb62a3e02fb8d60f0d9a117a5756
Richard Huang <rickypc@users.noreply.github.com>
2020-03-06 20:02:30 -0800

    Add more docs.

--- index.js
+++ index.js
@@ -6,11 +6,22 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 /**
- * Provide utility to fill out web form programmatically.
+ * Provide utility to fill out web form and mutate field data programmatically.
  *
  * @module form-mutator
  *
- * @example
+ * @example <caption>Browser usage</caption>
+ * <script type="text/javascript" src="form-mutator.min.js"></script>
+ * <script>
+ *   formMutator.fillOut({
+ *     '[name="fullName"]': 'Your Name',
+ *     '[name="address"]': '1 Awesome Way',
+ *     '[name="city"]': 'Lalaland',
+ *     '[type="submit"]': true,
+ *   });
+ * </script>
+ *
+ * @example <caption>Node.js usage</caption>
  * const formMutator = require('form-mutator');
  *
  * formMutator.fillOut({
@@ -22,20 +33,9 @@
  */
 ((global) => {
   /* istanbul ignore next */
-  exports = typeof exports !== 'undefined' ? exports : global.formMutator = {};
+  exports = typeof exports === 'undefined' ? global.formMutator = {} : exports;
   const evt = { bubbles: true, cancelable: true };
 
-  /**
-   * Click on given element.
-   *
-   * @alias module:form-mutator.click
-   * @param {Object} element - The element to click.
-   * @return {boolean} Truthy if click triggered, otherwise falsy.
-   *
-   * @example
-   * const el = document.querySelector('a[href="/"]');
-   * const response = formMutator.click(el);
-   */
   const click = (el) => {
     let response = false;
 
@@ -49,9 +49,6 @@
     return response;
   };
 
-  /**
-   * @private
-   */
   const toggleSelectboxByText = (el, values, selected) => {
     let response = false;
     let selections = Array.isArray(values) ? values : [values];
@@ -80,47 +77,9 @@
   };
 
   // After toggleSelectboxByText definition.
-  /**
-   * Deselect given values from dropdown list.
-   *
-   * @alias module:form-mutator.deselectByText
-   * @param {Object} element - The element to deselect from.
-   * @param {string[]} values - The values to deselect.
-   * @return {boolean} Truthy if deselect triggered, otherwise falsy.
-   *
-   * @example
-   * const el = document.querySelector('[name="month"]');
-   * const response = formMutator.deselectByText(el, ['January']);
-   */
   const deselectByText = (el, values) => toggleSelectboxByText(el, values, false);
-
-  /**
-   * Select given values from dropdown list.
-   *
-   * @alias module:form-mutator.selectByText
-   * @param {Object} element - The element to select from.
-   * @param {string[]} values - The values to select.
-   * @return {boolean} Truthy if select triggered, otherwise falsy.
-   *
-   * @example
-   * const el = document.querySelector('[name="month"]');
-   * const response = formMutator.selectByText(el, ['January']);
-   */
   const selectByText = (el, values) => toggleSelectboxByText(el, values, true);
 
-  /**
-   * Toggle the checkbox or radio button field.
-   *
-   * @alias module:form-mutator.toggleCheckbox
-   * @param {Object} element - The element to toggle from.
-   * @param {boolean} value - Check if true, otherwise uncheck.
-   * @return {boolean} Truthy if toggle triggered, otherwise falsy.
-   *
-   * @example
-   * const el = document.querySelector('[type="checkbox"]');
-   * const response1 = formMutator.toggleCheckbox(el, true);  // checked
-   * const response2 = formMutator.toggleCheckbox(el, false); // unchecked
-   */
   const toggleCheckbox = (el, value) => {
     let response = false;
 
@@ -137,18 +96,6 @@
     return response;
   };
 
-  /**
-   * Type given value to password or text field.
-   *
-   * @alias module:form-mutator.typeValue
-   * @param {Object} element - The element to type on.
-   * @param {mixed} value - The value to be typed in.
-   * @return {boolean} Truthy if type in triggered, otherwise falsy.
-   *
-   * @example
-   * const el = document.querySelector('[type="text"]');
-   * const response = formMutator.typeValue(el, 'value');
-   */
   const typeValue = (el, value) => {
     let response = false;
 
@@ -205,12 +152,110 @@
     return response;
   };
 
+  // After setValue definition.
+  const fillOut = (data = {}) => Object.keys(data).every((key) => setValue(key, data[key]));
+
+  /**
+   * Click on given element.
+   *
+   * @alias module:form-mutator.click
+   * @param {Object} element - The element to click.
+   * @return {boolean} Truthy if click triggered, otherwise falsy.
+   *
+   * @example
+   * const el = document.querySelector('a[href="/"]');
+   * const response = formMutator.click(el);
+   */
   exports.click = click;
+
+  /**
+   * Deselect given values from dropdown list.
+   *
+   * @alias module:form-mutator.deselectByText
+   * @param {Object} element - The element to deselect from.
+   * @param {string[]} values - The values to deselect.
+   * @return {boolean} Truthy if deselect triggered, otherwise falsy.
+   *
+   * @example
+   * const el = document.querySelector('[name="month"]');
+   * const response = formMutator.deselectByText(el, ['January']);
+   */
   exports.deselectByText = deselectByText;
-  exports.fillOut = (data = {}) => Object.keys(data)
-    .every((key) => setValue(key, data[key]));
+
+  /**
+   * Fill out all form field matched given selector => value key-pair. It will
+   * skip unknown field type and missing field.
+   *
+   * @alias module:form-mutator.fillOut
+   * @param {Object} data - The selector => value data map.
+   * @param {string} data.selector - The selector to find desired element.
+   * @param {mixed} data.value - The value to be set.
+   * @return {boolean} Truthy if the values are set, otherwise false.
+   *
+   * @example
+   * const response = formMutator.fillOut({
+   *   '[name="fullName"]': 'Your Name',
+   *   '[name="address"]': '1 Awesome Way',
+   *   '[name="city"]': 'Lalaland',
+   *   '[type="submit"]': true,
+   * });
+   */
+  exports.fillOut = fillOut;
+
+  /**
+   * Select given values from dropdown list.
+   *
+   * @alias module:form-mutator.selectByText
+   * @param {Object} element - The element to select from.
+   * @param {string[]} values - The values to select.
+   * @return {boolean} Truthy if select triggered, otherwise falsy.
+   *
+   * @example
+   * const el = document.querySelector('[name="month"]');
+   * const response = formMutator.selectByText(el, ['January']);
+   */
   exports.selectByText = selectByText;
+
+  /**
+   * Set given value to input field that match given selector.
+   *
+   * @alias module:form-mutator.setValue
+   * @param {string} selector - The selector to find desired element.
+   * @param {mixed} value - The value to be set.
+   * @return {boolean} Truthy if the value is set, or the field is unknown type
+   * or the field can not be found, otherwise false.
+   *
+   * @example
+   * const response = formMutator.setValue('[type="text"]', 'value');
+   */
   exports.setValue = setValue;
+
+  /**
+   * Toggle the checkbox or radio button field.
+   *
+   * @alias module:form-mutator.toggleCheckbox
+   * @param {Object} element - The element to toggle from.
+   * @param {boolean} value - Check if true, otherwise uncheck.
+   * @return {boolean} Truthy if toggle triggered, otherwise falsy.
+   *
+   * @example
+   * const el = document.querySelector('[type="checkbox"]');
+   * const response1 = formMutator.toggleCheckbox(el, true);  // checked
+   * const response2 = formMutator.toggleCheckbox(el, false); // unchecked
+   */
   exports.toggleCheckbox = toggleCheckbox;
+
+  /**
+   * Type given value to password or text field.
+   *
+   * @alias module:form-mutator.typeValue
+   * @param {Object} element - The element to type on.
+   * @param {mixed} value - The value to be typed in.
+   * @return {boolean} Truthy if type in triggered, otherwise falsy.
+   *
+   * @example
+   * const el = document.querySelector('[type="text"]');
+   * const response = formMutator.typeValue(el, 'value');
+   */
   exports.typeValue = typeValue;
-})(/* istanbul ignore next */typeof window !== 'undefined' ? window : global || this);
+})(/* istanbul ignore next */typeof window == 'undefined' ? global || this : window);
